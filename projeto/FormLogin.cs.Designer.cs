754655e908cdbb3d90598405647c603845c2bc27
Seu Martins <Baielsantana@gmail.com>
2026-06-17 09:27:15 -0300

    Feat: pasta services

---
+++ projeto/FormLogin.cs.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace projeto
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblLogin = new Label();
+            txtLogin = new TextBox();
+            lblSenha = new Label();
+            txtSenha = new TextBox();
+            btnEntrar = new Button();
+            SuspendLayout();
+            // 
+            // lblLogin
+            // 
+            lblLogin.AutoSize = true;
+            lblLogin.Location = new Point(314, 67);
+            lblLogin.Name = "lblLogin";
+            lblLogin.Size = new Size(49, 20);
+            lblLogin.TabIndex = 0;
+            lblLogin.Text = "Login:";
+            // 
+            // txtLogin
+            // 
+            txtLogin.Location = new Point(278, 110);
+            txtLogin.Name = "txtLogin";
+            txtLogin.Size = new Size(125, 27);
+            txtLogin.TabIndex = 1;
+            // 
+            // lblSenha
+            // 
+            lblSenha.AutoSize = true;
+            lblSenha.Location = new Point(314, 162);
+            lblSenha.Name = "lblSenha";
+            lblSenha.Size = new Size(52, 20);
+            lblSenha.TabIndex = 0;
+            lblSenha.Text = "Senha:";
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(278, 205);
+            txtSenha.Name = "txtSenha";
+            txtSenha.PasswordChar = '●';
+            txtSenha.Size = new Size(125, 27);
+            txtSenha.TabIndex = 1;
+            // 
+            // btnEntrar
+            // 
+            btnEntrar.Location = new Point(291, 252);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.Size = new Size(94, 29);
+            btnEntrar.TabIndex = 2;
+            btnEntrar.Text = "Entrar";
+            btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnEntrar);
+            Controls.Add(txtSenha);
+            Controls.Add(lblSenha);
+            Controls.Add(txtLogin);
+            Controls.Add(lblLogin);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblLogin;
+        private TextBox txtLogin;
+        private Label lblSenha;
+        private TextBox txtSenha;
+        private Button btnEntrar;
+    }
+}
---
+++ projeto/FormLogin.cs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace projeto
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void txtLogin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
